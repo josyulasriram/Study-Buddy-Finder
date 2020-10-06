aa677dce7e49781429a783099340d469ff6325bf
George Blov <georgeblov@Georges-MacBook-Pro.local>
2020-10-05 21:56:19 -0400

    bootstrap + UI

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'mysite',
-
+    'bootstrap4',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
