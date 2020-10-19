f3a9c4a6cf54f7a823c2fc05b257e4215aacb75e
Sriram Josyula <josyulasriram@gmail.com>
2020-10-18 20:52:59 -0400

    Update test_googleLogin.py

--- test/test_googleLogin.py
+++ test/test_googleLogin.py
@@ -17,3 +17,16 @@ class TestAuthWithGoogleAuth(unittest.TestCase):
     def tearDown(self):
         _auth.HAS_GOOGLE_AUTH = True
         _auth.HAS_OAUTH2CLIENT = True
+
+    def test_credentials_from_file(self):
+        with mock.patch(
+            "google.auth.load_credentials_from_file", autospec=True
+        ) as default:
+            default.return_value = (mock.sentinel.credentials, mock.sentinel.project)
+
+            credentials = _auth.credentials_from_file("credentials.json")
+
+            self.assertEqual(credentials, mock.sentinel.credentials)
+            default.assert_called_once_with(
+                "credentials.json", scopes=None, quota_project_id=None
+            )
