f09188c41c1d0fcb9bb72341695888d99910c2d7
Sriram Josyula <josyulasriram@gmail.com>
2020-10-19 00:17:37 -0400

    google login test

---
+++ test/test_googleLogin.py
@@ -0,0 +1,42 @@
+import mock
+
+import google.auth.credentials
+import google_auth_httplib2
+import httplib2
+import oauth2client.client
+import unittest2 as unittest
+
+from googleapiclient import _auth
+
+
+class TestAuthWithGoogleAuth(unittest.TestCase):
+    def setUp(self):
+        _auth.HAS_GOOGLE_AUTH = True
+        _auth.HAS_OAUTH2CLIENT = False
+
+    def tearDown(self):
+        _auth.HAS_GOOGLE_AUTH = True
+        _auth.HAS_OAUTH2CLIENT = True
+
+    def test_credentials_from_file(self):
+        with mock.patch(
+            "google.auth.load_credentials_from_file", autospec=True
+        ) as default:
+            default.return_value = (mock.sentinel.credentials, mock.sentinel.project)
+
+            credentials = _auth.credentials_from_file("credentials.json")
+
+            self.assertEqual(credentials, mock.sentinel.credentials)
+
+    def test_credentials_from_file(self):
+        with mock.patch(
+            "google.auth.load_credentials_from_file", autospec=True
+        ) as default:
+            default.return_value = (mock.sentinel.credentials, mock.sentinel.project)
+
+            credentials = _auth.credentials_from_file("credentials.json")
+
+            self.assertEqual(credentials, mock.sentinel.credentials)
+            default.assert_called_once_with(
+                "credentials.json", scopes=None, quota_project_id=None
+            )
--- test/test_trial.py
+++
@@ -1,2 +0,0 @@
-def test_trial():
-  assert (1+1) == 2, "Test failed"
