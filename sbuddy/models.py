30b21eed56edb0b6d2ef02a54207c27fe88dcb9b
George Blov <georgeblov@Georges-MacBook-Pro.local>
2020-10-27 16:25:59 -0400

    reset migrations

--- sbuddy/models.py
+++ sbuddy/models.py
@@ -6,3 +6,4 @@ class User(models.Model):
     strengths = models.CharField(max_length=255, blank=False)
     skills = models.CharField(max_length=255, blank=False)
     schedule = models.FileField(upload_to='schedules/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
