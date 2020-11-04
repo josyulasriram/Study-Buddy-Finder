eba072b68d2837d7038e1c44651d2398973f52d6
George Blov <georgeblov@Georges-MacBook-Pro.local>
2020-11-03 20:23:48 -0500

    travis tests

--- sbuddy/tests.py
+++ sbuddy/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from sbuddy.models import User
+import sbuddy.views as views
+
+
+class UserModelTests(TestCase):
+
+    def test_match_strengths(self):
+        personA = User("George", "Mathematics", "Reading, Writing", None, timezone.now())
+        personB = User("Lily", "Mathematics", "English", None, timezone.now())
+
+        self.assertIs(views.do_items_match(personA.strengths, personB.strengths), True)
+
+    def test_no_match_strengths(self):
+        personA = User("George", "English", "Reading, Writing", None, timezone.now())
+        personB = User("Lily", "Mathematics", "English", None, timezone.now())
+
+        self.assertIs(views.do_items_match(personA.strengths, personB.strengths), False)
+
+
+    def test_no_match_skills(self):
+        personA = User("George", "Mathematics", "Reading, Writing", None, timezone.now())
+        personB = User("Lily", "Mathematics", "English", None, timezone.now())
+
+        self.assertIs(views.do_items_match(personA.skills, personB.skills), False)
+
+    def test_match_skills(self):
+        personA = User("George", "Mathematics", "Reading, Writing", None, timezone.now())
+        personB = User("Katie", "English", "Reading", None, timezone.now())
+
+        self.assertIs(views.do_items_match(personA.skills, personB.skills), True)
