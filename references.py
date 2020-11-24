d2c8a2e0ad50e52c193390fd05945d814c1d5070
Sriram Josyula <josyulasriram@gmail.com>
2020-11-24 14:20:55 -0500

    Update references.py

--- references.py
+++ references.py
@@ -1,17 +1,19 @@
 """
+
 References
-Title: OAuth 2.0
-Authors: Multiple, public repository on github: https://github.com/aaronpk/oauth.net
-Date: 9/15
-Code Version: 2.0
-URL: https://oauth.net/2/
-Software License: Apache
 
 Title:Bootstrap 4
 Authors: https://github.com/orgs/twbs/people (List of 10 primary authors)
 Date: 9/18
 Code Version: 4.5
 URL: https://getbootstrap.com/
+Software License: MIT
+
+Title: Crispy Forms
+Authors: Miguel Araujo
+Date: 11/15
+Code Version:1.10.0
+URL: https://pypi.org/project/django-crispy-forms/#description
 Software License: MIT
 
 Title: django-phonenumber-field
@@ -21,18 +23,32 @@ Code Version: 1
 URL: https://github.com/stefanfoulis/django-phonenumber-field
 Software License: MIT
 
-Title: Twilio
-Authors: Multiple authors in the company, Jeff Lawson CEO
-Date: 11/15
-Code Version: 6.48.0
-URL: https://pypi.org/project/twilio/
+Title: djago-allauth
+Authors: https://github.com/pennersr/django-allauth/graphs/contributors
+Date: 9/30
+Code Version:1
+URL: https://www.intenct.nl/projects/django-allauth/
 Software License: MIT
+
+Title: OAuth 2.0
+Authors: Multiple, public repository on github: https://github.com/aaronpk/oauth.net
+Date: 9/15
+Code Version: 2.0
+URL: https://oauth.net/2/
+Software License: Apache
 
 Title:Pillow
 Authors: Alex Clark(primary maintainer)
 Date: 10/29
 Code Version:8.0.1 
 URL: https://pypi.org/project/Pillow/#history
+Software License: MIT
+
+Title: Twilio
+Authors: Multiple authors in the company, Jeff Lawson CEO
+Date: 11/15
+Code Version: 6.48.0
+URL: https://pypi.org/project/twilio/
 Software License: MIT
 
 """
